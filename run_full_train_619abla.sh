97b0b649a013f3ffbc422232a0166e14478e898b
yudongbo1991 <yudongbo@ucas.ac.cn>
2026-07-02 17:50:13 +0800

    Update run_full_train_619abla.sh

--- run_full_train_619abla.sh
+++ run_full_train_619abla.sh
@@ -15,30 +15,30 @@ nvcc --version
 
 # 4. 执行顺序训练（增加 -u 确保日志实时刷新 HC+HH）
 echo "Starting abla_1-only dual branch Task..."
-python -u train_GW-RWKV.py --config configs_619abla/sw_hanchuan_1.yaml
+python -u train_GW-VRWKV.py --config configs_619abla/sw_hanchuan_1.yaml
 
-python -u train_GW-RWKV.py --config configs_619abla/sw_honghu_1.yaml
+python -u train_GW-VRWKV.py --config configs_619abla/sw_honghu_1.yaml
 
 echo "Starting abla_2-dual branch+fusion Task..."
-python -u train_GW-RWKV.py --config configs_619abla/sw_hanchuan_2.yaml
+python -u train_GW-VRWKV.py --config configs_619abla/sw_hanchuan_2.yaml
 
-python -u train_GW-RWKV.py --config configs_619abla/sw_honghu_2.yaml
+python -u train_GW-VRWKV.py --config configs_619abla/sw_honghu_2.yaml
 
 echo "Starting abla_3-only ipm Task..."
-python -u train_GW-RWKV.py --config configs_619abla/sw_hanchuan_3.yaml
+python -u train_GW-VRWKV.py --config configs_619abla/sw_hanchuan_3.yaml
 
-python -u train_GW-RWKV.py --config configs_619abla/sw_honghu_3.yaml
+python -u train_GW-VRWKV.py --config configs_619abla/sw_honghu_3.yaml
 
 
 echo "Starting abla_4-ipm+dual branch(no fusion) Task..."
-python -u train_GW-RWKV.py --config configs_619abla/sw_hanchuan_4.yaml
+python -u train_GW-VRWKV.py --config configs_619abla/sw_hanchuan_4.yaml
 
-python -u train_GW-RWKV.py --config configs_619abla/sw_honghu_4.yaml
+python -u train_GW-VRWKV.py --config configs_619abla/sw_honghu_4.yaml
 
 echo "Starting full Task..."
-python -u train_GW-RWKV.py --config configs_619abla/sw_hanchuan_full.yaml
+python -u train_GW-VRWKV.py --config configs_619abla/sw_hanchuan_full.yaml
 
-python -u train_GW-RWKV.py --config configs_619abla/sw_honghu_full.yaml
+python -u train_GW-VRWKV.py --config configs_619abla/sw_honghu_full.yaml
 
 echo "HC+HH训练任务完成！"
 
@@ -52,30 +52,30 @@ echo "HC+HH训练任务完成！"
 
 # 5. 执行顺序训练（增加 -u 确保日志实时刷新 IP+XZ）
 echo "Starting abla_1-only dual branch Task..."
-python -u train_GW-RWKV.py --config configs_619abla/sw_indian_1.yaml
+python -u train_GW-VRWKV.py --config configs_619abla/sw_indian_1.yaml
 
-python -u train_GW-RWKV.py --config configs_619abla/sw_xuzhou_1.yaml
+python -u train_GW-VRWKV.py --config configs_619abla/sw_xuzhou_1.yaml
 
 echo "Starting abla_2-dual branch+fusion Task..."
-python -u train_GW-RWKV.py --config configs_619abla/sw_indian_2.yaml
+python -u train_GW-VRWKV.py --config configs_619abla/sw_indian_2.yaml
 
-python -u train_GW-RWKV.py --config configs_619abla/sw_xuzhou_2.yaml
+python -u train_GW-VRWKV.py --config configs_619abla/sw_xuzhou_2.yaml
 
 echo "Starting abla_3-only ipm Task..."
-python -u train_GW-RWKV.py --config configs_619abla/sw_indian_3.yaml
+python -u train_GW-VRWKV.py --config configs_619abla/sw_indian_3.yaml
 
-python -u train_GW-RWKV.py --config configs_619abla/sw_xuzhou_3.yaml
+python -u train_GW-VRWKV.py --config configs_619abla/sw_xuzhou_3.yaml
 
 
 echo "Starting abla_4-ipm+dual branch(no fusion) Task..."
-python -u train_GW-RWKV.py --config configs_619abla/sw_indian_4.yaml
+python -u train_GW-VRWKV.py --config configs_619abla/sw_indian_4.yaml
 
-python -u train_GW-RWKV.py --config configs_619abla/sw_xuzhou_4.yaml
+python -u train_GW-VRWKV.py --config configs_619abla/sw_xuzhou_4.yaml
 
 echo "Starting full Task..."
-python -u train_GW-RWKV.py --config configs_619abla/sw_indian_full.yaml
+python -u train_GW-VRWKV.py --config configs_619abla/sw_indian_full.yaml
 
-python -u train_GW-RWKV.py --config configs_619abla/sw_xuzhou_full.yaml
+python -u train_GW-VRWKV.py --config configs_619abla/sw_xuzhou_full.yaml
 
 echo "IP+XZ训练任务完成！"
 
